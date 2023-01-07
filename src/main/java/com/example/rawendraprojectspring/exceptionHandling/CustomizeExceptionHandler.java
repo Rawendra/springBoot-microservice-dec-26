68b712054fb042cf2c3836418ca02f4a4a8557e6
rawendra <rawendra@gmail.com>
2023-01-07 22:23:59 +0530

    adding custom expection

--- src/main/java/com/example/rawendraprojectspring/exceptionHandling/CustomizeExceptionHandler.java
+++ src/main/java/com/example/rawendraprojectspring/exceptionHandling/CustomizeExceptionHandler.java
@@ -10,16 +10,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.example.rawendraprojectspring.controller.NoUserFoundException;
+
 @ControllerAdvice
 public class CustomizeExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception{
-		
-		ErrorDetails err= new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDateTime.now());
-		
-		return new ResponseEntity(err, HttpStatus.INTERNAL_SERVER_ERROR);
-		
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
+
+		ErrorDetails err = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+
+	@ExceptionHandler(NoUserFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request)
+			throws Exception {
+
+		ErrorDetails err = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+
 	}
 
 }
